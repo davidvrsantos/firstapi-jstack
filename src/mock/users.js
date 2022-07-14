6bb5007c4b3dd158760a46cba1bf3c016506e4b0
David Santos <davidvrsantos@gmail.com>
2022-07-13 23:03:50 -0300

    content for study purposes

---
+++ src/mock/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'David'
+    },
+    {
+        id: 2,
+        name: 'David 2'
+    },
+    {
+        id: 3,
+        name: 'David 3'
+    },
+]
